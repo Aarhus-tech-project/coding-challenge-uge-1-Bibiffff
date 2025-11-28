e6764b617acf2cbceb6f33903583dd6143067f03
Bibi Bakmann <bibibakmann1@gmail.com>
2025-11-28 10:03:31 +0100

    tilføjet snake som game mode

--- numberguess/Program.cs
+++ numberguess/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading;
 
 namespace numberguess
 {
@@ -6,11 +8,11 @@ namespace numberguess
     {
         static void Main(string[] args)
         {
-            Console.Title = "Number Guessing Game";
+            Console.Title = "Number Guessing Game + Snake";
 
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("==========================================");
-            Console.WriteLine("        * NUMBER GUESSING GAME *");
+            Console.WriteLine("     * SNAKE + NUMBER GUESSING GAME *");
             Console.WriteLine("==========================================");
             Console.ResetColor();
 
@@ -32,19 +34,30 @@ namespace numberguess
 
             Console.WriteLine(" 1) Programmet gætter dit tal");
             Console.WriteLine(" 2) Du gætter programmets tal");
+            Console.WriteLine(" 3) Snake");
             Console.Write("\nDit valg: ");
 
             string choice = Console.ReadLine();
-
             if (choice == "1")
             {
                 ProgramGuesses game = new ProgramGuesses();
                 game.Play();
             }
-            else
+            else if (choice == "2")
             {
                 UserGuesses game = new UserGuesses();
                 game.Play();
+            }
+            else if (choice == "3")
+            {
+                SnakeGame game = new SnakeGame();
+                game.Play();
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Ugyldigt valg.");
+                Console.ResetColor();
             }
 
             Console.ForegroundColor = ConsoleColor.Cyan;
@@ -55,7 +68,7 @@ namespace numberguess
         }
     }
 
-    // MODE: PROGRAMMET GÆTTER BRUGERENS TAL
+    // MODE 1: PROGRAMMET GÆTTER BRUGERENS TAL
 
     class ProgramGuesses
     {
@@ -89,13 +102,8 @@ namespace numberguess
 
                 switch (response)
                 {
-                    case "h":
-                        min = guess + 1;
-                        break;
-
-                    case "l":
-                        max = guess - 1;
-                        break;
+                    case "h": min = guess + 1; break;
+                    case "l": max = guess - 1; break;
 
                     case "j":
                         guessed = true;
@@ -134,7 +142,7 @@ namespace numberguess
         }
     }
 
-    // MODE: BRUGEREN GÆTTER PROGRAMMETS TAL
+    // MODE 2: BRUGEREN GÆTTER PROGRAMMETS TAL
 
     class UserGuesses
     {
@@ -196,6 +204,170 @@ namespace numberguess
                 Console.Write("Ikke et tal! Prøv igen: ");
                 Console.ResetColor();
             }
+        }
+    }
+
+    // MODE 3: SNAKE GAME MED SCOREBOARD
+
+    class SnakeGame
+    {
+        enum Direction { Up, Down, Left, Right }
+
+        public void Play()
+        {
+            Console.Clear();
+            Console.CursorVisible = false;
+
+            int width = 40;
+            int height = 20;
+            int score = 0;
+
+            DrawBorder(width, height);
+            ShowScore(score);
+
+            var snake = new LinkedList<(int x, int y)>();
+            snake.AddFirst((width / 2, height / 2));
+            snake.AddLast((width / 2 - 1, height / 2));
+            snake.AddLast((width / 2 - 2, height / 2));
+
+            Direction dir = Direction.Right;
+
+            Random rand = new Random();
+            (int x, int y) food = PlaceFood(rand, width, height, snake);
+            Draw(food.x, food.y, '*');
+
+            bool alive = true;
+            int delay = 120;
+
+            while (alive)
+            {
+                // Input
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKey key = Console.ReadKey(true).Key;
+                    dir = ChangeDirection(dir, key);
+                }
+
+                // New head
+                var head = snake.First.Value;
+                var newHead = head;
+
+                switch (dir)
+                {
+                    case Direction.Up: newHead.y--; break;
+                    case Direction.Down: newHead.y++; break;
+                    case Direction.Left: newHead.x--; break;
+                    case Direction.Right: newHead.x++; break;
+                }
+
+                // Collisions
+                if (newHead.x <= 0 || newHead.x >= width - 1 ||
+                    newHead.y <= 1 || newHead.y >= height - 1)
+                    alive = false;
+
+                foreach (var s in snake)
+                    if (s.x == newHead.x && s.y == newHead.y)
+                        alive = false;
+
+                if (!alive) break;
+
+                // Move snake
+                snake.AddFirst(newHead);
+                Draw(newHead.x, newHead.y, 'O');
+
+                // FOOD
+                if (newHead == food)
+                {
+                    score++;
+                    ShowScore(score);
+
+                    food = PlaceFood(rand, width, height, snake);
+                    Draw(food.x, food.y, '*');
+                }
+                else
+                {
+                    var tail = snake.Last.Value;
+                    snake.RemoveLast();
+                    Draw(tail.x, tail.y, ' ');
+                }
+
+                Thread.Sleep(delay);
+            }
+
+            // GAME OVER
+            Console.CursorVisible = true;
+            Console.SetCursorPosition(0, height + 1);
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("\nGame Over!");
+            Console.ResetColor();
+            Console.WriteLine($"Din score: {score}");
+        }
+
+        // ----- Helpers -----
+
+        Direction ChangeDirection(Direction current, ConsoleKey key)
+        {
+            if (key == ConsoleKey.UpArrow && current != Direction.Down) return Direction.Up;
+            if (key == ConsoleKey.DownArrow && current != Direction.Up) return Direction.Down;
+            if (key == ConsoleKey.LeftArrow && current != Direction.Right) return Direction.Left;
+            if (key == ConsoleKey.RightArrow && current != Direction.Left) return Direction.Right;
+
+            return current;
+        }
+
+        void Draw(int x, int y, char c)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(c);
+        }
+
+        void ShowScore(int score)
+        {
+            Console.SetCursorPosition(2, 0);
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write($"            Score: {score}   ");
+            Console.ResetColor();
+        }
+
+        void DrawBorder(int width, int height)
+        {
+            Console.Clear();
+
+            // Top border with scoreboard space
+            for (int x = 0; x < width; x++)
+                Draw(x, 1, '#');
+
+            // Bottom
+            for (int x = 0; x < width; x++)
+                Draw(x, height - 1, '#');
+
+            // Sides
+            for (int y = 1; y < height; y++)
+            {
+                Draw(0, y, '#');
+                Draw(width - 1, y, '#');
+            }
+        }
+
+        (int x, int y) PlaceFood(Random r, int width, int height, LinkedList<(int x, int y)> snake)
+        {
+            int x, y;
+            bool taken;
+
+            do
+            {
+                x = r.Next(1, width - 1);
+                y = r.Next(2, height - 1);
+
+                taken = false;
+
+                foreach (var s in snake)
+                    if (s.x == x && s.y == y)
+                        taken = true;
+
+            } while (taken);
+
+            return (x, y);
         }
     }
 }
